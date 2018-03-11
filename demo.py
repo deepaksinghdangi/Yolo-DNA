7929d34cdcfd1d6be711e12959e9067a8f080216
deepaksinghdangi <32464590+deepaksinghdangi@users.noreply.github.com>
2018-03-11 15:06:55 +0530

    supress

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
 import sys
 
 sys.path.append('./')
-sys.path.append('./')
 sys.path.append('./yolo')
 sys.path.append('./yolo/dataset')
 sys.path.append('./yolo/net')
@@ -9,23 +8,107 @@ sys.path.append('./yolo/solver')
 sys.path.append('./yolo/utils')
 
 from yolo.net.yolo_tiny_net import YoloTinyNet 
-import tensorflow as tf 
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
 import cv2
 import numpy as np
-import colorsys
-import imghdr
-from PIL import Image, ImageDraw, ImageFont
 import random
 from keras import backend as K
 import math
+import colorsys
+import imghdr
+import os
+import random
+import scipy.misc
+from PIL import Image, ImageDraw, ImageFont
 
 classes_name =  ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train","tvmonitor"]
+
+
+
+def read_classes(classes_path):
+    with open(classes_path) as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+    return class_names
+
+def read_anchors(anchors_path):
+    with open(anchors_path) as f:
+        anchors = f.readline()
+        anchors = [float(x) for x in anchors.split(',')]
+        anchors = np.array(anchors).reshape(-1, 2)
+    return anchors
+
+def generate_colors(class_names):
+    hsv_tuples = [(x / len(class_names), 1., 1.) for x in range(len(class_names))]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    random.seed(10101)  # Fixed seed for consistent colors across runs.
+    random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
+    random.seed(None)  # Reset seed to default.
+    return colors
+
+def scale_boxes(boxes, image_shape):
+    """ Scales the predicted boxes in order to be drawable on the image"""
+    height = image_shape[0]
+    width = image_shape[1]
+    image_dims = K.stack([height, width, height, width])
+    image_dims = K.reshape(image_dims, [1, 4])
+    boxes = boxes * image_dims
+    return boxes
+
+def preprocess_image(img_path, model_image_size):
+    image_type = imghdr.what(img_path)
+    image = Image.open(img_path)
+    resized_image = image.resize(tuple(reversed(model_image_size)), Image.BICUBIC)
+    image_data = np.array(resized_image, dtype='float32')
+    image_data /= 255.
+    image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+    return image, image_data
+
+def draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors):
+    #Loading the font File
+    font = ImageFont.truetype(font='FiraMono-Medium.otf',size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+    thickness = (image.size[0] + image.size[1]) // 300
+   
+    print("Drawing boxes...")
+    for i, c in reversed(list(enumerate(out_classes))):
+        predicted_class = class_names[c]
+        box = out_boxes[i]
+        score = out_scores[i]
+     
+
+        label = '{} {:.2f}'.format(predicted_class, score)
+
+        draw = ImageDraw.Draw(image)
+        label_size = draw.textsize(label, font)
+
+        top, left, bottom, right = box
+        top = max(0, np.floor(top + 0.5).astype('int32'))
+        left = max(0, np.floor(left + 0.5).astype('int32'))
+        bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+        right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+        print(label, (left, top), (right, bottom))
+
+        if top - label_size[1] >= 0:
+            text_origin = np.array([left, top - label_size[1]])
+        else:
+            text_origin = np.array([left, top + 1])
+
+        # My kingdom for a good redistributable image drawing library.
+        for i in range(thickness):
+            draw.rectangle([left + i, top + i, right - i, bottom - i], outline=colors[c])
+        draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=colors[c])
+        draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+        del draw
+
 
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
 # Interpret network output (7x7x30) to bb coordinates (long process)
-def InterpretPredictions(predicts):
+def InterpretPredictions(predicts,sess):
   p_classes = predicts[0, :, :, 0:20] # conditional class probabilitites -> 7x7x20
   C = predicts[0, :, :, 20:22]        # individual box confidence predictions -> 7x7x2 
   coordinate = predicts[0, :, :, 22:] # class coordinates -> 7x7x8
@@ -33,73 +116,65 @@ def InterpretPredictions(predicts):
   p_classes = np.reshape(p_classes, (7, 7, 1, 20))
   C = np.reshape(C, (7, 7, 2, 1))
  
+  threshold = 0.13
   # multiply class probabilities with confidence matrix
-  P = C * p_classes
-
+  P = C * p_classes   #7x7x2x20
   P = sigmoid(P)  # normalize them into probabilities
 
-  scores = P[P>0.52]
-  print("scores shape: "+str(scores.shape))
-  find_probs = np.where(P>0.52) #  Find the most cells witht Probabilities > Threshold
-  #print(find_probs)
-  find_probs_np = []
+  box_scores = C * p_classes    #7x7x2x20
+  box_scores = tf.convert_to_tensor(box_scores)
 
-  # Initialize labels to return
-  # 20: max_objects_per_image
-  labels = [[0, 0, 0, 0, 0]] * 20
-  object_num = 0
+  box_classes = K.argmax(box_scores,-1)    #7x7x2
+  box_class_scores = K.max(box_scores,-1)  #7x7x2
+  
+  print(sess.run(box_classes), sess.run(box_class_scores))
+  filtering_mask = box_class_scores > threshold
+  
+  print(sess.run(filtering_mask))
+    
+  index = tf.where(box_class_scores > threshold) #  Find the most cells witht Probabilities > Threshold
 
-  # Read the elements
-  for i in range(len(find_probs)):  
-    find_probs_np.append(find_probs[i])
+  #print(sess.run(index))
 
-  for j in range(0, len(find_probs_np[0])):
+  scores = tf.boolean_mask(box_class_scores,filtering_mask)
+  boxes = tf.boolean_mask(coordinate,filtering_mask)
+  classes = tf.boolean_mask(box_classes,filtering_mask)
+  
+  print(sess.run(scores),sess.run(boxes),sess.run(classes))
+  # Scale boxes back to original image shape
+  #boxes = scale_boxes(boxes, (300.,300.))
 
-    myindex=[] # myindex is for all the objects in the image
+  max_boxes = 10
+  max_boxes_tensor = K.variable(max_boxes, dtype='int32')     # tensor to be used in tf.image.non_max_suppression()
+  print(sess.run(boxes))
+  sess.run(tf.variables_initializer([max_boxes_tensor])) # initialize variable max_boxes_tensor
+  
+  #iou_threshold = 0.5
+  #nms_indices = tf.image.non_max_suppression(boxes,scores,max_boxes,0.5)
+  #scores = K.gather(scores,nms_indices)
+  #boxes = K.gather(boxes,nms_indices)
+  #classes = K.gather(classes,nms_indices)
 
-    for i in range(0, len(find_probs_np)):         
-      myindex.append(find_probs_np[i][j])
+  return scores, boxes, classes, index
 
-    index = np.argmax(P) # Get the elements for the class with the highest probabilities (this returns only one object)
-    index = np.unravel_index(index, P.shape)
-
-    # Get the class number
-    class_num = myindex[3]
-
-    # Get the coordinates 
-    max_coordinate = coordinate[myindex[0], myindex[1], myindex[2], :]
-
-    # Get x,y center of the BB
-    xcenter = max_coordinate[0]
-    ycenter = max_coordinate[1]
-    w = max_coordinate[2]
-    h = max_coordinate[3]
-    xcenter = (myindex[1] + xcenter) * (448/7.0)
-    ycenter = (myindex[0] + ycenter) * (448/7.0)
-
-    w = w * 448
-    h = h * 448
-
-    # Convert them to BB coordinates (same as annotations)
-    xmin = xcenter - w/2.0
-    ymin = ycenter - h/2.0
-    xmax = xmin + w
-    ymax = ymin + h
-
-    # Append to the final labels
-    labels[object_num] = [xmin, ymin, xmax, ymax, class_num]
-    object_num += 1
-    if object_num >= 20:
-          break
-  return labels, object_num, scores
-
-#  Felzenszwalb et al.
-def non_max_suppression(boxes, overlapThresh):
+# import the necessary packages
+import numpy as np
+ 
+# Malisiewicz et al.
+def non_max_suppression(boxes, object_num, overlapThresh):
     # if there are no boxes, return an empty list
+    boxes = np.array(boxes,dtype=np.float32)
+    boxes = boxes[:object_num,:]
+   
     if len(boxes) == 0:
         return []
-
-    # initialize the list of picked indexes
+ 
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+ 
+    # initialize the list of picked indexes	
     pick = []
 
     # grab the coordinates of the bounding boxes
@@ -107,55 +182,89 @@ def non_max_suppression(boxes, overlapThresh):
     y1 = boxes[:,1]
     x2 = boxes[:,2]
     y2 = boxes[:,3]
-
+ 
+    print(boxes)
+    
     # compute the area of the bounding boxes and sort the bounding
     # boxes by the bottom-right y-coordinate of the bounding box
     area = (x2 - x1 + 1) * (y2 - y1 + 1)
-    idxs = np.argsort(y2)
 
+    idxs = np.argsort(y2)
     # keep looping while some indexes still remain in the indexes
     # list
+    print("AREA:",area) 
+    print("IDXS:",idxs)
     while len(idxs) > 0:
-        # grab the last index in the indexes list, add the index
-        # value to the list of picked indexes, then initialize
-        # the suppression list (i.e. indexes that will be deleted)
-        # using the last index
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
         last = len(idxs) - 1
         i = idxs[last]
+        print("HELLO",i)
         pick.append(i)
-        suppress = [last]
-        # loop over all indexes in the indexes list
-        for pos in range(0, last):
-            # grab the current index
-            j = idxs[pos]
-            # find the largest (x, y) coordinates for the start of
-            # the bounding box and the smallest (x, y) coordinates
-            # for the end of the bounding box
-            xx1 = max(x1[i], x1[j])
-            yy1 = max(y1[i], y1[j])
-            xx2 = min(x2[i], x2[j])
-            yy2 = min(y2[i], y2[j])
+ 
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        print("x1[i]:",x1[i])
+        print("x1[idxs[:last]]:",x1[idxs[:last]])
+        
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+        
+        print(idxs[:last])
+ 
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+ 
+        # compute the ratio of overlap
+        
+        overlap = (w * h) / area[idxs[:last]]
 
-            # compute the width and height of the bounding box
-            w = max(0, xx2 - xx1 + 1)
-            h = max(0, yy2 - yy1 + 1)
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+            np.where(overlap > overlapThresh)[0])))
+    # return only the bounding boxes that were picked using the
+    # integer data type
+    return boxes[pick].astype("int")
 
-            # compute the ratio of overlap between the computed
-            # bounding box and the bounding box in the area list
-            overlap = float(w * h) / area[j]
 
-            # if there is sufficient overlap, suppress the
-            # current bounding box
-            if overlap > overlapThresh:
-                suppress.append(pos)
 
-        # delete all indexes from the index list that are in the
-        # suppression list
-        idxs = np.delete(idxs, suppress)
+def scale(out_scores, out_boxes, out_classes, index):
+    labels = [[0, 0, 0, 0, 0]] * 20
+    object_num = 0
+    for i, c in reversed(list(enumerate(out_classes))):
+        predicted_class = classes_name[c]
+        max_coordinate = out_boxes[i]
+        score = out_scores[i]
+        
+        #Extract the box coordinates
+        xcenter = max_coordinate[0]
+        ycenter = max_coordinate[1]
+        w = max_coordinate[2]
+        h = max_coordinate[3]
 
-    # return only the bounding boxes that were picked
-    return boxes[pick]
-
+        xcenter = (index[i][1] + xcenter) * (448/7.0)
+        ycenter = (index[i][0] + ycenter) * (448/7.0)
+        w = w * 448
+        h = h * 448
+        
+        # Convert them to BB coordinates (same as annotations)
+        xmin = xcenter - w/2.0
+        ymin = ycenter - h/2.0
+        xmax = xmin + w
+        ymax = ymin + h
+        
+        #print(xmin,ymin,xmax,ymax,c)
+        
+        labels[object_num] = [xmin, ymin, xmax, ymax, c]
+        object_num += 1
+        if object_num >= 20:
+              break
+    return labels, object_num
+        
 common_params = {'image_size': 448, 'num_classes': 20, 
                 'batch_size':1}
 net_params = {'cell_size': 7, 'boxes_per_cell':2, 'weight_decay': 0.0005}
@@ -163,9 +272,14 @@ net_params = {'cell_size': 7, 'boxes_per_cell':2, 'weight_decay': 0.0005}
 net = YoloTinyNet(common_params, net_params, test=True)
 
 image = tf.placeholder(tf.float32, (1, 448, 448, 3))
+
 predicts = net.inference(image)
 
 sess = tf.Session()
+
+saver = tf.train.Saver(net.trainable_collection)
+
+saver.restore(sess, 'models/pretrain/yolo_tiny.ckpt')
 
 np_img = cv2.imread('cat.jpg')
 resized_img = cv2.resize(np_img, (448, 448))
@@ -177,38 +291,30 @@ np_img = np_img.astype(np.float32)
 np_img = np_img / 255.0 * 2 - 1
 np_img = np.reshape(np_img, (1, 448, 448, 3))
 
-saver = tf.train.Saver(net.trainable_collection)
-
-saver.restore(sess, 'models/pretrain/yolo_tiny.ckpt')
-
 np_predict = sess.run(predicts, feed_dict={image: np_img})
 
 #xmin, ymin, xmax, ymax, class_num= process_predicts(np_predict)
-labels, object_num, scores = InterpretPredictions(np_predict)
+out_scores, out_boxes, out_classes, index = InterpretPredictions(np_predict,sess)
 
-print("BEFORE")
+# Print predictions info
+out_scores, out_boxes, out_classes, index = sess.run([out_scores, out_boxes, out_classes, index])
+
+iou_threshold = 0.6
+
+labels,object_num = scale(out_scores,out_boxes,out_classes,index)
+
+labels = non_max_suppression(labels, object_num, iou_threshold)
+
+np_img = cv2.imread('cat.jpg')
+resized_img = cv2.resize(np_img, (448, 448))
+np_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+
 for obj_num,object in enumerate(labels):
-    if obj_num == object_num:
-        break
-    print(object)
-    class_name = classes_name[int(object[4])]
-    print("BEFORE: Printing Classes: ",str(class_name))
-
-objects = non_max_suppression(np.asarray(labels,dtype=np.float32),0.8)
-print(objects)
-
-for obj_num,object in enumerate(objects):
-    if obj_num == object_num:
-        break
-    class_name = classes_name[int(object[4])]
+    class_name = classes_name[object[4]]
     print("Printing Classes: ",str(class_name))
     cv2.rectangle(resized_img, (int(object[0]), int(object[1])), (int(object[2]), int(object[3])), (0, 0, 255))
     cv2.putText(resized_img, class_name, (int(object[0]), int(object[1])), 2, 1.5, (0, 0, 255))
-'''
-class_name2 = classes_name[class_num2]
-cv2.rectangle(resized_img, (int(xmin2), int(ymin2)), (int(xmax2), int(ymax2)), (0, 0, 255))
-cv2.putText(resized_img, class_name2, (int(xmin2), int(ymin2)), 2, 1.5, (0, 0, 255))
-'''
+
 cv2.imwrite('cat_out.jpg', resized_img)
 
 sess.close()
